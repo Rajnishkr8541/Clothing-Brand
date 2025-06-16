87e3b718a5aca60a2cab8b8b618e04907072c079
Rajnish kumar <100439674+Rajnishkr8541@users.noreply.github.com>
2025-06-16 11:11:34 +0530

    login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -61,3 +61,79 @@ export async function registerUserController(req, res) {
     }
 
 }
+
+export async function verifyEmailController(req, res){
+    try {
+        const {code} = req.body
+
+        const user = await UserModel.findOne({_id: code})
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Invalid verification code',
+                error: true,
+                success: false
+            })
+        }
+    const updatedUser = await UserModel.updateOne({_id : code},{
+        verify_email: true
+    })
+        return res.json({
+            message: 'Email verified successfully',
+            error: false,
+            success: true,
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+            error: true,
+            success: false
+        })
+    }
+}
+
+//LOGIN
+
+export async function loginController(req, res){
+    try {
+        const {email, password}= req.body
+
+        const user = await UserModel.findOne({email})
+        if (!user) {
+            return res.status(400).json({
+                message: 'User not found',
+                error: true,
+                success: false
+            })
+        }
+
+        if(user.status!== "Active"){
+            return res.status(400).json({
+                message: 'Account is not active',
+                error: true,
+                success: false
+            })
+        }
+
+        const checkPassword = await bcryptjs.compare(password, user.password)
+
+        if(!checkPassword){
+            res.status(400).json({
+                message: 'Invalid credentials',
+                error: true,
+                success: false
+            })
+        }
+
+
+
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message || error,
+            error: true,
+            success: false
+        })
+    }
+}
